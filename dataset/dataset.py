83b5531eade3c0b3f50d93449d64dfd01993961a
21522173huy <tulehuy.003@gmail.com>
2024-10-23 20:18:58 +0700

    change more

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,5 +1,3 @@
-
-
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -7,8 +5,6 @@ from datasets import load_from_disk
 
 class PolarityMapping:
     INDEX_TO_POLARITY = { 0: None, 1: 'positive', 2: 'negative', 3: 'neutral' }
-    # INDEX_TO_ONEHOT = { i: [1 if i == j else 0 for j in INDEX_TO_POLARITY] for i in INDEX_TO_POLARITY } 
-    # POLARITY_TO_INDEX = { polarity: index for index, polarity in INDEX_TO_POLARITY.items() }
     POLARITY_TO_INDEX = { None: 0, 'positive': 1, 'negative': 2, 'neutral': 3 }
 
 class VLSPDataset(Dataset):
@@ -28,30 +24,28 @@ class VLSPDataset(Dataset):
 
         # Labels
         labels = []
-        for i, each in enumerate(item):
-            if i == 34: break
+        for i, each in enumerate(self.aspect_names):
             labels.append(item[each])
 
         inputs = {
-            'input_ids' : self.data['input_ids'],
-            'attention_mask' : self.data['attention_mask'],
-            'labels' : torch.tensor(labels)
+            'input_ids': torch.tensor(item['input_ids']),
+            'attention_mask': torch.tensor(item['attention_mask']),
+            'labels': torch.tensor(labels)
         }
 
         return inputs
 
     def idx2polarity(self, label):
-        # Implement the logic for swapping tensor values if needed
         res = []
         for i, each in enumerate(label):
             item = {
                 'Aspect': self.aspect_names[i],
-                'Polarity': self.mapping.INDEX_TO_POLARITY[each]
+                'Polarity': self.mapping.INDEX_TO_POLARITY[each.item()]
             }
             res.append(item)
         return res
     
-def create_dataset(data_path, batch_size = 16):
+def create_dataset(data_path, batch_size=16):
     # Load the dataset
     dataset = load_from_disk(data_path)
     
