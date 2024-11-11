893ae9bf2bfb1e17a03a9d692984e1533fe5acd2
LeHuyTu <101401285+21522173huy@users.noreply.github.com>
2024-11-11 20:28:03 +0700

    Update early_stopping.py

--- early_stopping.py
+++ early_stopping.py
@@ -14,7 +14,7 @@ class EarlyStopping:
         self.delta = delta
         self.path = path
 
-    def __call__(self, val_loss, model):
+    def __call__(self, val_loss, model, optimizer, scheduler):
         score = -val_loss
 
         if self.best_score is None:
@@ -28,12 +28,16 @@ class EarlyStopping:
                 self.early_stop = True
         else:
             self.best_score = score
-            self.save_checkpoint(val_loss, model)
+            self.save_checkpoint(val_loss, model, optimizer, scheduler)
             self.counter = 0
 
-    def save_checkpoint(self, val_loss, model):
+    def save_checkpoint(self, val_loss, model, optimizer, scheduler):
         '''Saves model when validation loss decrease.'''
         if self.verbose:
             print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        torch.save(model.state_dict(), self.path)
+        torch.save({
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'scheduler_state_dict': scheduler.state_dict(),
+            }, self.path)
         self.val_loss_min = val_loss
